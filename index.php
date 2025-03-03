d237596b5acfe59a25b53dda082c8fc8302e679e
Adela Máčajová <adela.macajova3@gmail.com>
2025-03-03 18:55:47 +0100

    Prva verzia

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nadpis</h1>
+    <?php
+        //sem pis kod
+        echo "<h1>Moja stránka</h1>"; //dvojité úvodzovky
+        echo 'Moja stránka<br>'; //jednoduché úvodzovky
+        echo ("Moja stránka<br>"); //echo moze mat aj zatvorky
+    ?>
+</body>
+</html>
