da18774d06ac294ab3fba296ad7db469c3304b99
Adela Máčajová <adela.macajova3@gmail.com>
2025-03-10 18:02:54 +0100

    prva-verzia

--- index.php
+++ index.php
@@ -1,17 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Nadpis</h1>
-    <?php
-        //sem pis kod
-        echo "<h1>Moja stránka</h1>"; //dvojité úvodzovky
-        echo 'Moja stránka<br>'; //jednoduché úvodzovky
-        echo ("Moja stránka<br>"); //echo moze mat aj zatvorky
-    ?>
-</body>
-</html>
+
+    
+   <?php
+      include('partials/header.php'); //require namiesto include - vobec mi neukaze stranku ak mam niekde chybu, ak napisem paartials a neexistuje to tak include mi ukaze stranku bez stylov re
+   ?> 
+    <main>
+      <section class="slides-container">
+        <div class="slide fade">
+          <img src="img/carousel/carousel.jpg">
+          <div class="slide-text">
+            Prvý nadpis
+          </div>
+        </div>
+        
+        <div class="slide fade">
+          <img src="img/carousel/carousel2.jpg">
+          <div class="slide-text">
+            Druhý nadpis
+          </div>
+        </div>
+        
+        <div class="slide fade">
+          <img src="img/carousel/carousel3.jpg">
+          <div class="slide-text">
+            Tretí nadpis
+          </div>
+        </div>
+        
+        <a id="prev" class="prev">❮</a>
+        <a id="next" class="next">❯</a>
+        
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <p><strong><em>Elit culpa id mollit irure sit. Ex ut et ea esse culpa officia ea incididunt elit velit veniam qui. Mollit deserunt culpa incididunt laborum commodo in culpa.</em></strong></p>
+          </div>
+        </div>
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-50">
+            <h2>Mollit cupidatat velit quis irure non eiusmod culpa cillum velit magna est aliquip.</h2>
+          </div>
+          <div class="col-50">
+            <p>Nulla dolore sit esse pariatur culpa sint nulla fugiat nulla ut. Sit adipisicing fugiat id consequat qui est. Nulla ea aliquip culpa nulla mollit do excepteur ex ullamco consequat tempor consectetur. Tempor et ea voluptate irure est magna magna reprehenderit qui non dolore. Consequat id ad cillum do qui. Proident ipsum enim eiusmod fugiat quis anim consequat ullamco mollit excepteur. Eiusmod eu ad irure anim velit laborum commodo nostrud pariatur id ea ad.</p>
+            <p>Incididunt pariatur ipsum in culpa sit cillum consequat nostrud do. Esse commodo ad qui ea. Pariatur elit mollit deserunt aute do culpa eu veniam pariatur in adipisicing qui adipisicing irure. Est minim sint ipsum nulla eiusmod. Amet do officia consectetur labore. Id aute labore culpa aliquip. Adipisicing mollit excepteur Lorem quis elit ipsum irure quis occaecat qui consectetur ad irure dolor.</p>
+          </div>
+        </div>
+      </section>
+    </main>
+    
+<?php
+  include('partials/footer.php');
+?>  
